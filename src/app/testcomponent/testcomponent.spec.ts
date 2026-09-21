4bfddd824ed2070fa90f370bd7bc26970091ea87
unknown <antoinecarrette@hotmail.fr>
2026-09-21 11:48:27 +0200

    build: component architecture test

---
+++ src/app/testcomponent/testcomponent.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Testcomponent } from './testcomponent';
+
+describe('Testcomponent', () => {
+  let component: Testcomponent;
+  let fixture: ComponentFixture<Testcomponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Testcomponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Testcomponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/testcomponent/testcomponent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testcomponent',
+  imports: [],
+  templateUrl: './testcomponent.html',
+  styleUrl: './testcomponent.css',
+})
+export class Testcomponent {}
