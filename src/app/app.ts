0bd2269098e827874a5e2b8172d59738d715c0ce
unknown <antoinecarrette@hotmail.fr>
2026-09-24 19:17:53 +0200

    build:header component creation 24/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Testcomponent } from './testcomponent/testcomponent';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Testcomponent],
+  imports: [RouterOutlet, Testcomponent, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
