c307356380f0de2ded18f6a7c856fae55f310410
unknown <antoinecarrette@hotmail.fr>
2026-09-26 11:07:02 +0200

    build: generating products grid and cards 26/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Testcomponent } from './testcomponent/testcomponent';
 import { Header } from './header/header';
+import { ProductGrid } from './products/product-grid/product-grid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Testcomponent, Header],
+  imports: [RouterOutlet, Testcomponent, Header, ProductGrid],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
