d9e53037d60abac8e6798ba8cdfc85190cc9f2cf
unknown <antoinecarrette@hotmail.fr>
2026-09-26 11:32:02 +0200

    build: product grid list of product 26/09/2026

--- src/app/products/product-grid/product-grid.ts
+++ src/app/products/product-grid/product-grid.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-grid',
@@ -7,4 +8,28 @@ import { ProductCard } from '../product-card/product-card';
   templateUrl: './product-grid.html',
   styleUrl: './product-grid.css',
 })
-export class ProductGrid {}
+export class ProductGrid {
+  protected readonly products = signal<Product[]>([
+    {
+      id: 1,
+      name: "untel",
+      description:"untel",
+      price : 12,
+      originalPrice: 12
+    },
+    {
+      id: 2,
+      name: "untel",
+      description:"untel",
+      price : 12,
+      originalPrice: 12
+    },
+    {
+      id: 3,
+      name: "untel",
+      description:"untel",
+      price : 12,
+      originalPrice: 12
+    },
+  ])
+}
